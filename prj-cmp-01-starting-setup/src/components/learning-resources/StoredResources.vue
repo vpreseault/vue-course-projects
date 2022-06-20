730b4bef0f1a3d0fcb495cd0d4a4268c7aabd03f
Vincent Preseault <vincent.preseault@gmail.com>
2022-06-20 06:18:46 -0400

    Lecture 1-3 - dummy resources

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resources>
+  </ul>
+</template>
+<script>
+import LearningResources from './LearningResources.vue';
+
+export default {
+  components: {
+    LearningResources,
+  },
+  props: ['resources'],
+  data() {
+    return {
+      storedResources: this.resources,
+    };
+  },
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
